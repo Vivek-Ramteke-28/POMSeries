c83fbf0a5ba9af2119556592c2c033a927c6de6b
Vivek Ramteke <vivek.r@toranainc.com>
2021-05-17 19:32:17 +0530

    added code as per feedback

--- src/main/java/com/qa/hubspot/pages/signUp.java
+++ src/main/java/com/qa/hubspot/pages/signUp.java
@@ -1,10 +1,20 @@
 package com.qa.hubspot.pages;
 
+import org.openqa.selenium.By;
+
 public class signUp {
 
+	
+	By signUp = By.id("test");
 	
 	public void signUpMethod()
 	{
 		System.out.println("signUpMethod");
+		signUpMethodDetails();
+	}
+	
+	public boolean signUpMethodDetails()
+	{
+		return true;
 	}
 }
